631c418d3d4b6d5f9e85c92ce3a7bee7b413673c
熊长江 <343196441@qq.com>
2016-07-04 15:18:00 +0800

    修改下

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config.js');
-config.entry.app.unshift("webpack-dev-server/client?http://localhost:8888/","webpack/hot/dev-server");
+config.entry.app.unshift("webpack-dev-server/client?http://0.0.0.0:8888/","webpack/hot/dev-server");
 new WebpackDevServer(webpack(config),{
   hot: true,
   noInfo: false,
@@ -9,7 +9,7 @@ new WebpackDevServer(webpack(config),{
   stats: {
     colors: true
   }
-}).listen(8888, 'localhost', function(err){
+}).listen(8888, '0.0.0.0', function(err){
   if(err){
     console.log(err);
   }

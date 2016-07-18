228c6088b8e3371c9fe80d44d7cc5b87f68bf818
lianghaibin <haibin.liang@mangocity.com>
2016-07-18 11:40:33 +0800

    共公资源添加

--- html/tanker/index.js
+++ html/tanker/index.js
@@ -27,3 +27,46 @@ var swiperDBanner = new Swiper('.mg-dbanner',{
 	loop: true,
 	autoplay: 3000
 })
+
+
+$("#load-btn").on("tap",function(){
+	init.loading(4);
+})
+
+$("#alert-btn1").on("tap",function(){
+	init.tips();
+})
+
+var init={};
+init.loading=function(t){
+	var showHtml='<div class="mg-mark"></div><div class="mg-loading"><img src="/i/loading.gif" /></div>'
+
+	$('body').append(showHtml);
+
+	$(".mg-mark").on("tap",function(){
+		$(".mg-mark,.mg-loading").remove();
+	})
+
+	setTimeout(function(){
+		$(".mg-mark,.mg-loading").remove();
+	},3*1000)
+
+}
+
+init.tips=function(){
+	var alertHtml='<div class="mg-mark"></div>'+
+        '<div class="mg_tips mg-fs28">'+
+            '<h3 class="mg_tips_tit mg-fs32">提示</h3>'+
+            '<div class="mg_tips_text">提示的内容信息提示的内容信息提示的内容信息提示的内容信息</div>'+
+            '<div class="mg_tips_btn g">'+
+                '<a class="col col2" href="">提交</a>'+
+                '<a class="col col2" href="">重试</a>'+
+            '</div>'+
+        '</div>';
+    $('body').append(alertHtml);
+
+	$(".mg-mark").on("tap",function(){
+		$(".mg-mark,.mg_tips").remove();
+	})
+
+}

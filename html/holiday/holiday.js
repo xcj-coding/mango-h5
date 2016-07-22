11b1ae255265138fa898ab5a3f1eb8a2c55e5937
lianghaibin <haibin.liang@mangocity.com>
2016-07-22 15:08:50 +0800

    首页与列表页

---
+++ html/holiday/holiday.js
@@ -0,0 +1,9 @@
+$(".mg-swiper-img").show();
+var swiperlmzq = new Swiper('.mg-hot-swiper',{
+	wrapperClass : 'mg-hot-list',
+	slideClass : 'mg-hot-box',
+	pagination : '.mg-hot-ico',
+	bulletActiveClass : 'mg-hot-active',
+	loop: true,
+	autoplay: 30000
+})
